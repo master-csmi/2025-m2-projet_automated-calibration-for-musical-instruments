fix conservative aspect of the numerical scheme update report images and performance tables

dg_solver/mass_matrix.py
@@ -1,10 +1,7 @@
 import jax.numpy as jnp
 
-def local_mass_inv_system(h, S_cell, c=1.0, S_star=1.0):
+def local_mass_inv_system(h):
     M_ref = (h / 6.0) * jnp.array([[2., 1.],
                                   [1., 2.]])
 
-    Mp = (S_cell / (c * S_star)) * M_ref
-    Mv = (S_star / (c * S_cell)) * M_ref
-
-    return jnp.linalg.inv(Mp), jnp.linalg.inv(Mv)
+    return jnp.linalg.inv(M_ref), jnp.linalg.inv(M_ref)

dg_solver/rhs.py
@@ -9,67 +9,55 @@ from utils.flux import rusanov_flux
 # ------------------------------------------------------------------------------------------------------------------------------
 #                                                          local volume term for system
 # ------------------------------------------------------------------------------------------------------------------------------
-def local_volume_system(u_cell, xL, xR, A, nq=24):
+def local_volume_system(u_cell, xL, xR, nq=24):
     h = xR - xL
     xq = jnp.linspace(xL, xR, nq)
     w  = jnp.ones(nq) * (h/(nq-1))
     w  = w.at[0].set(h/(2*(nq-1)))
     w  = w.at[-1].set(h/(2*(nq-1)))
 
-    # reconstruction U(x)
-    phi_q = vphi_at(xq, xL, xR)       
-    p_q = phi_q @ u_cell[0]           
-    v_q = phi_q @ u_cell[1]           
-    Uq = jnp.stack([p_q, v_q], axis=1)
+    phi_q = vphi_at(xq, xL, xR)
+    p_q = phi_q @ u_cell[0]
+    v_q = phi_q @ u_cell[1]
 
-    Fq = Uq @ A.T                    
+    Fq = jnp.stack([v_q, p_q], axis=1)
 
-    # exact derivative of basis functions
+    # dérivées exactes des fonctions de base
     dphi0 = -1.0 / h
     dphi1 =  1.0 / h
 
-    # Integration of flux * dphi
+    # Intégration
     V0 = jnp.sum(w[:,None] * Fq * dphi0, axis=0)
     V1 = jnp.sum(w[:,None] * Fq * dphi1, axis=0)
 
-    return jnp.stack([V0, V1], axis=1)   
+    return jnp.stack([V0, V1], axis=1)  
 
-
-v_local_volume_system = jax.vmap(local_volume_system, in_axes=(0, 0, 0, None, None))
 
 def surface_term_system(u_ext, j, c=1.0):
-    """
-    Compute DG surface term for cell j with S(x) variable.
-
-    u_ext   : array (N+2,2,2) with ghost cells
-    S_ext   : array (N+2,) S at nodes/interfaces
-    j       : cell index in original u_cells
-    """
-    jp = j + 1  # offset due to ghost cell
+    jp = j + 1
 
     # Left interface
-    UL_left  = u_ext[jp-1, :, 1]  # right node of left cell
-    UR_left  = u_ext[jp,   :, 0]  # left node of current cell
-   
-    
+    UL_left  = u_ext[jp-1, :, 1]
+    UR_left  = u_ext[jp,   :, 0]
+
 
     # Right interface
-    UL_right = u_ext[jp,   :, 1]  # right node of current cell
-    UR_right = u_ext[jp+1, :, 0]  # left node of right cell
-    
+    UL_right = u_ext[jp,   :, 1]
+    UR_right = u_ext[jp+1, :, 0]
 
+
+    # Flux Rusanov conservatif
     f_left  = rusanov_flux(UL_left, UR_left, c)
     f_right = rusanov_flux(UL_right, UR_right, c)
-    # assemble surface term (2x2)
+
     S_term = jnp.zeros((2,2))
     S_term = S_term.at[:,0].set(-f_left)
     S_term = S_term.at[:,1].set( f_right)
     return S_term
 
 
-v_surface_term_system = jax.vmap(surface_term_system, in_axes=(None, 0, None))
 
-def dg_rhs_system(u_cells, x_nodes, S_cells, c, A, Mp_inv, Mv_inv, bc, phi, beta, Z, alpha):
+def dg_rhs_system(u_cells, x_nodes, c, Mp_inv, Mv_inv, bc, phi, beta, Z, alpha):
     xLs, xRs = cell_edges_from_nodes(x_nodes)
     N = u_cells.shape[0]
 
@@ -78,9 +66,7 @@ def dg_rhs_system(u_cells, x_nodes, S_cells, c, A, Mp_inv, Mv_inv, bc, phi, beta
         u_ext = apply_bc(u_cells, bc.left, phi, beta, Z, alpha)
     elif bc.type == "neumann":
         u_ext = apply_bc_neumann(u_cells)
-
-    # S at nodes/interfaces with ghost cells
-    S_nodes_ext = jnp.concatenate([S_cells[:1], S_cells, S_cells[-1:]])  
+ 
 
     # Surface term (fluxes)
     S_all = jax.vmap(
@@ -89,7 +75,7 @@ def dg_rhs_system(u_cells, x_nodes, S_cells, c, A, Mp_inv, Mv_inv, bc, phi, beta
 
     # Volume term
     V_all = jax.vmap(
-        lambda Ue, xL, xR: local_volume_system(Ue, xL, xR, A, 24)
+        lambda Ue, xL, xR: local_volume_system(Ue, xL, xR)
     )(u_cells, xLs, xRs)
 
     # assemble RHS cell by cell